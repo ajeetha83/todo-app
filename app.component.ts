a5105e076402a0dbbc1994921d4763c4286e458e
ajeetha83 <82981140+ajeetha83@users.noreply.github.com>
2021-05-29 16:57:28 -0500

    generated deployables

--- app.component.ts
+++ app.component.ts
@@ -18,6 +18,7 @@ export class AppComponent implements OnInit {
     constructor( private renderer : Renderer2, private router: Router, @Inject(DOCUMENT,) private document: any, private element : ElementRef, public location: Location) {}
     ngOnInit() {
         var navbar : HTMLElement = this.element.nativeElement.children[0].children[0];
+        // @ts-ignore
         this._router = this.router.events.filter(event => event instanceof NavigationEnd).subscribe((event: NavigationEnd) => {
             if (window.outerWidth > 991) {
                 window.document.children[0].scrollTop = 0;
